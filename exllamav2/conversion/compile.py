fe51a8f4b52769f7526d5e23e974b090c63b8fe8
turboderp <11859846+turboderp@users.noreply.github.com>
2025-03-14 23:15:15 +0100

    Correctly include Q/K norms when compiling model

--- exllamav2/conversion/compile.py
+++ exllamav2/conversion/compile.py
@@ -97,6 +97,10 @@ def compile_model(job, save_fn, model):
                 d = get_q_module(job, module.k_proj); out_dict.update(d); current_size += _dsize(d)
                 d = get_q_module(job, module.v_proj); out_dict.update(d); current_size += _dsize(d)
                 d = get_q_module(job, module.o_proj); out_dict.update(d); current_size += _dsize(d)
+                d = get_f_module(job, module.q_norm)
+                if d: out_dict.update(d); current_size += _dsize(d)
+                d = get_f_module(job, module.k_norm)
+                if d: out_dict.update(d); current_size += _dsize(d)
 
             if isinstance(module, ExLlamaV2MLP):
 
