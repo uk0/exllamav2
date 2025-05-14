1adff7d827062eb2f426f812070f9966f2ad0e54
turboderp <11859846+turboderp@users.noreply.github.com>
2025-05-14 15:30:57 +0200

    Remove SentencePiece support

--- exllamav2/tokenizer/__init__.py
+++ exllamav2/tokenizer/__init__.py
@@ -1,5 +1,4 @@
 from exllamav2.version import __version__
 
 from exllamav2.tokenizer.base import ExLlamaV2TokenizerBase
-from exllamav2.tokenizer.spm import ExLlamaV2TokenizerSPM
 from exllamav2.tokenizer.hf import ExLlamaV2TokenizerHF
